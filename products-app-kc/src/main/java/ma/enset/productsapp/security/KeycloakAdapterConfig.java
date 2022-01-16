41cbed03a8e816a21dc01f8cd78629f4034f4a31
sakout-reda <sakout-reda@hotmail.fr>
2022-01-16 22:23:06 +0100

    commit

--- products-app-kc/src/main/java/ma/enset/productsapp/security/KeycloakAdapterConfig.java
+++ products-app-kc/src/main/java/ma/enset/productsapp/security/KeycloakAdapterConfig.java
@@ -1,6 +1,8 @@
 package ma.enset.productsapp.security;
 
 import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +11,10 @@ public class KeycloakAdapterConfig {
     @Bean
     public KeycloakSpringBootConfigResolver springBootConfigResolver(){
         return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Bean
+    KeycloakRestTemplate keycloakRestTemplate(KeycloakClientRequestFactory keycloakClientRequestFactory){
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
     }
 }
--- products-app-kc/src/main/java/ma/enset/productsapp/web/ProductController.java
+++ products-app-kc/src/main/java/ma/enset/productsapp/web/ProductController.java
@@ -1,15 +1,39 @@
 package ma.enset.productsapp.web;
 
+import lombok.Data;
 import ma.enset.productsapp.repositories.ProductRepository;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.AdapterDeploymentContext;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.spi.HttpFacade;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.facade.SimpleHttpFacade;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ProductController{
     @Autowired
+    private KeycloakRestTemplate keycloakRestTemplate;
+
+    @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private AdapterDeploymentContext adapterDeploymentContext;
 
     @GetMapping("/")
     public String index(){
@@ -21,8 +45,39 @@ public class ProductController{
         return "products";
     }
     @GetMapping("/suppliers")
-    public String suppliers(){
+    public String suppliers(Model model){
+        PagedModel<Supplier> pageSuppliers = keycloakRestTemplate.getForObject("http://localhost:8083/suppliers", PagedModel.class);
+        model.addAttribute("suppliers", pageSuppliers);
         return "suppliers";
     }
 
+    @GetMapping("/jwt")
+    @ResponseBody
+    public Map<String,String> map(HttpServletRequest request){
+        KeycloakAuthenticationToken token = (KeycloakAuthenticationToken) request.getUserPrincipal();
+        KeycloakPrincipal principal=(KeycloakPrincipal)token.getPrincipal();
+        KeycloakSecurityContext keycloakSecurityContext = principal.getKeycloakSecurityContext();
+        Map<String,String> map = new HashMap<>();
+        map.put("access_token", keycloakSecurityContext.getIdTokenString());
+        return map;
+
+    }
+
+    @GetMapping("/profile")
+    public String profile(RedirectAttributes attributes, HttpServletRequest request, HttpServletResponse response){
+        HttpFacade httpFacade = new SimpleHttpFacade(request, response);
+        KeycloakDeployment deployment = adapterDeploymentContext.resolveDeployment(httpFacade);
+        attributes.addAttribute("referrer", deployment.getResourceName());
+        attributes.addAttribute("referrer_uri", request.getHeader("referer"));
+        return "redirect:"+deployment.getAccountUrl();
+
+    }
+
+}
+
+@Data
+class Supplier {
+    private Long id;
+    private String name;
+    private String email;
 }
